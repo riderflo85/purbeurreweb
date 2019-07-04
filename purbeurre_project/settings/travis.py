79d17bc7e13bb05287245e334eb8c4c7087c1f52
Florent <grenaille.florent85@gmail.com>
2019-07-04 10:48:38 +0200

    Ajout du fichier 'travis.py' et modif de la conf de travis

---
+++ purbeurre_project/settings/travis.py
@@ -0,0 +1,15 @@
+from . import *
+
+
+SECRET_KEY="sdbdfbjdfDsvsfdbfdsEJZH03"
+
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': '',
+    'USER': 'postgres',
+    'PASSWORD': '',
+    'HOST': '',
+    'PORT': '',
+    },
+}
