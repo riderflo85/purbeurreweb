49e6f190e42d5e04ddbe812851dc4e77f435902d
Florent <grenaille.florent85@gmail.com>
2019-07-10 17:36:35 +0200

    Ajout des scripts pour la mise a jour de la BDD

---
+++ search/management/commands/updatefood.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand, CommandError
+from search.complete_db import main
+from search.update_db import update_database
+
+
+class Command(BaseCommand):
+    help = "Update database food content"
+
+    def handle(self, *args, **options):
+        """ execute an action according to the command """
+
+        cat = [
+            "boissons", "cereales-et-derives", "desserts", "fruits",
+            "legumes-et-derives", "poissons", "produits-laitiers", "viandes"
+        ]
+
+        food = main()
+        result = update_database(cat, food)
+
+        if result:
+            self.stdout.write(self.style.SUCCESS(
+                'La mise à jour de la base de données est terminée.'
+            ))
+        
+        else:
+            self.stdout.write(self.style.ERROR(
+                'Une erreur est survenu au niveau de la base de données.'
+            ))
